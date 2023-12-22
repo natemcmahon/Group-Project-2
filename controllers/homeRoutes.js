ddce632c9d1f472680413967284b7071c47b91fc
Nate McMahon <nathaniel.mcmahon@hrblock.com>
2023-12-21 21:06:37 -0600

    Set up initial routes for homepage, profile and login. User is able to access homepage without login, but will be redirected to login if they try to access profile.handlebars

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,62 @@
+const router = require('express').Router();
+const { Post, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+  try {
+    // Get all image posts and JOIN with user data
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    // render the homepage, pass posts and a logged in parameter containing the value "logged_in" 
+    // as part of the request.session
+    res.render('homepage', { 
+      posts, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Use withAuth middleware to prevent access to profile page
+router.get('/profile', withAuth, async (req, res) => {
+    try {
+      // Find the logged in user based on the session ID
+      const userData = await User.findByPk(req.session.user_id, {
+        attributes: { exclude: ['password'] },
+        include: [{ model: Post }],
+      });
+  
+      const user = userData.get({ plain: true });
+  
+      res.render('profile', {
+        ...user,
+        logged_in: true
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to the user's profile page
+    if (req.session.logged_in) {
+      res.redirect('/profile');
+      return;
+    }
+  
+    res.render('login');
+  });
+  
+  module.exports = router;
+  
