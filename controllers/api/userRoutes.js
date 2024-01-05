531a72fb229810ad49880ca4c32a58ad255941f0
Amanda Norris <amandakathleennorris@gmail.com>
2024-01-04 21:17:43 -0600

    work on login route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -18,6 +18,7 @@ router.post('/', async (req, res) => {
 
 router.post('/login', async (req, res) => {
   try {
+    console.log('hi');
     const userData = await User.findOne({ where: { email: req.body.email } });
 
     if (!userData) {
@@ -44,6 +45,7 @@ router.post('/login', async (req, res) => {
     });
 
   } catch (err) {
+    console.log(err);
     res.status(400).json(err);
   }
 });
